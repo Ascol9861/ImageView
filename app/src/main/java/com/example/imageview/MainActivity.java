712dd861215c00c44185d0decb47afd5a053361e
Ascol9861 <ascolparajuli65@gmail.com>
2019-04-01 11:28:23 +0545

    Image Viewer used

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rdoSingle = findViewById(R.id.rdoSingle);
         rdoDay = findViewById(R.id.rdoDay);
         rdoNight = findViewById(R.id.rdoNight);
+        imgOutput = findViewById(R.id.imgOutput);
 
         rdoSingle.setOnClickListener(this);
         rdoDay.setOnClickListener(this);
@@ -30,18 +31,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.rdoSingle:
-                Toast toast = Toast.makeText(this,"Single", Toast.LENGTH_LONG);
-                toast.show();
+//                Toast toast = Toast.makeText(this,"Single", Toast.LENGTH_LONG);
+//                toast.show();
+                imgOutput.setImageResource(R.drawable.single);
                 break;
 
             case R.id.rdoDay:
-                Toast toast1 = Toast.makeText(this,"Day", Toast.LENGTH_SHORT);
-                toast1.show();
+//                Toast toast1 = Toast.makeText(this,"Day", Toast.LENGTH_SHORT);
+//                toast1.show();
+                imgOutput.setImageResource(R.drawable.day);
                 break;
 
             case R.id.rdoNight:
-                Toast toast2 = Toast.makeText(this,"Night", Toast.LENGTH_LONG);
-                toast2.show();
+//                Toast toast2 = Toast.makeText(this,"Night", Toast.LENGTH_LONG);
+//                toast2.show();
+                imgOutput.setImageResource(R.drawable.night);
                 break;
 
         }
