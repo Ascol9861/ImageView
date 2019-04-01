9bd57151b98d981193198fa3159af7984eb176ff
Ascol9861 <ascolparajuli65@gmail.com>
2019-04-01 11:21:11 +0545

    Toast used

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,15 +2,48 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private RadioButton btnOne, btnTwo, btnThree;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private RadioButton rdoSingle, rdoDay, rdoNight;
+    private ImageView imgOutput;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rdoSingle = findViewById(R.id.rdoSingle);
+        rdoDay = findViewById(R.id.rdoDay);
+        rdoNight = findViewById(R.id.rdoNight);
+
+        rdoSingle.setOnClickListener(this);
+        rdoDay.setOnClickListener(this);
+        rdoNight.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.rdoSingle:
+                Toast toast = Toast.makeText(this,"Single", Toast.LENGTH_LONG);
+                toast.show();
+                break;
+
+            case R.id.rdoDay:
+                Toast toast1 = Toast.makeText(this,"Day", Toast.LENGTH_SHORT);
+                toast1.show();
+                break;
+
+            case R.id.rdoNight:
+                Toast toast2 = Toast.makeText(this,"Night", Toast.LENGTH_LONG);
+                toast2.show();
+                break;
+
+        }
     }
 }
